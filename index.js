003a930b51798ca61212b2738f6687da9a147106
Edwin Andermyr <andermyredwin@gmail.com>
2024-02-26 22:05:58 +0100

    updated timer with JS live clock with set interval 1000ms

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ import express from "express";
 const app = express(); 
 const port = 3000; 
 
-
+app.set('view engine', 'ejs');
 app.use(express.static("public"));
 // this line below will activate bootstrap 
 app.use('/node_modules', express.static('node_modules'));
 
 app.get("/", (req, res) => { 
-    res.render("index.ejs"); 
+     const currentDate = new Date();
+  const formattedDate = currentDate.toLocaleString(); 
+    res.render("index.ejs", {currentDate: formattedDate}); 
 })
 
 app.listen(port, () => {
