3278f231b540619893f7653611515de3004ae578
Edwin Andermyr <andermyredwin@gmail.com>
2024-02-29 10:56:13 +0100

    fixed the Vintage Router and tested the route well

--- routes/vintage.js
+++ routes/vintage.js
@@ -1,8 +1,14 @@
 import express from "express";
 const vintageRouter = express.Router();
 
+vintageRouter.use(express.static("public"));
+vintageRouter.use('/node_modules', express.static('node_modules'));
+
 vintageRouter.get("/", (req, res) => {
-  res.render("../views/vintage.ejs")
+      const currentDate = new Date();
+    const formattedDate = currentDate.toLocaleString(); 
+    
+  res.render("../views/vintage.ejs", {currentDate: formattedDate})
 });
 
 vintageRouter.get("/buycheap", (req, res) => {
