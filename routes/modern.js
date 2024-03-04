ba525798ffa9d0f1f052e7b7dee1e9a18ebdb2d3
Edwin Andermyr <andermyredwin@gmail.com>
2024-03-04 22:38:43 +0100

    establishing the same vintage logic with the modern page

--- routes/modern.js
+++ routes/modern.js
@@ -1,4 +1,5 @@
 import express from "express";
+import axios from "axios"
 const modernRouter = express.Router();
 
    const currentDate = new Date();
@@ -7,9 +8,24 @@ const formattedDate = currentDate.toLocaleString();
 modernRouter.use(express.static("public"));
 modernRouter.use('/node_modules', express.static('node_modules'));
 
-modernRouter.get("/", (req, res) => {
+modernRouter.get("/", async (req, res) => {
+      try {
+      // Searching within the Murders at Karlov Manor (MKM) - new set / still isolating page 1 with 80 results 
+    const response = await axios.get("https://api.scryfall.com/cards/search?q=set:mkm&page=1&per_page=80");
+      const allCards = response.data.data; 
       
-  res.render("../views/modern.ejs", {currentDate: formattedDate})
+    const filteredCards = allCards
+      .filter(card => card.type_line.toLowerCase().includes("land") === false) // Exclude lands
+      .filter(card => card.image_uris && card.image_uris.normal); // Exclude cards without images
+
+    // Limit the number of cards to 10
+    const slicedCards = filteredCards.slice(0, 12);
+
+    res.render("../views/modern.ejs", { currentDate: formattedDate, data: slicedCards });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("../views/modern.ejs", { currentDate: formattedDate, error: error.message });
+  }
 });
 
 modernRouter.get("/commander", (req, res) => {
