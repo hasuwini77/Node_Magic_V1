2a0eef2f8ef216b6000d9df98931207a60f4762e
Edwin Andermyr <andermyredwin@gmail.com>
2024-03-07 15:24:35 +0100

    random card displayed in Home logic working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express"; 
 import vintageRouter from "./routes/vintage.js";
 import modernRouter from "./routes/modern.js";
+import axios from "axios";
 
 const app = express(); 
 const port = process.env.PORT || 3000
@@ -10,11 +11,23 @@ app.use(express.static("public"));
 // this line below will activate bootstrap 
 app.use('/node_modules', express.static('node_modules'));
 
-app.get("/", (req, res) => { 
+app.get("/", async (req, res) => { 
      const currentDate = new Date();
   const formattedDate = currentDate.toLocaleString(); 
-    res.render("index.ejs", {currentDate: formattedDate}); 
-})
+  // Adding an API logic to create a fun random card using Scryfall 
+  try {
+    // Searching for a Random Magic Card 
+    const response = await axios.get("https://api.scryfall.com/cards/random");
+    const randomCard = response.data;
+
+    console.log(randomCard);
+    res.render("index.ejs", { currentDate: formattedDate, data: randomCard });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+
+    res.render("index.ejs", { currentDate: formattedDate, error: error.message });
+  }
+});
 
 app.use('/vintage', vintageRouter);
 app.use('/modern', modernRouter);
